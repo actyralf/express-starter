33a323724e9e7d3574d807efe83629f4b3296956
Ralf Siewert <ralf@actyvy.st>
2023-10-10 12:35:04 +0200

    set up handlers folder

--- server.js
+++ server.js
@@ -3,15 +3,14 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
+const { rootHandler } = require("./handlers/root");
 
 const port = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to the Express API" });
-});
+app.get("/", rootHandler);
 
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
